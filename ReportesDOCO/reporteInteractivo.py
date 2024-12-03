f7c10a3097a528fcf107e6a8075cc13071e716b4
Tu Nombre <36548944@ipscorrientes.local>
2024-12-03 14:34:11 -0300

    reporteInteractivo.py Modificado

--- ReportesDOCO/reporteInteractivo.py
+++ ReportesDOCO/reporteInteractivo.py
@@ -10,10 +10,10 @@ username = 'IPSCOR\36548944'
 password = ''
 
 
-    # Crear una cadena de conexión utilizando ODBC Driver 18
-#conn_str = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes; Encrypt=no'
-conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER=IPSDB-Replica;DATABASE=DoCo;Trusted_Connection=yes; Encrypt=no')
-#conn = pyodbc.connect(conn_str)
+# Crear una cadena de conexión utilizando ODBC Driver 18
+conn_str = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes; Encrypt=no'
+#conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER=IPSDB-Replica;DATABASE=DoCo;Trusted_Connection=yes; Encrypt=no')
+conn = pyodbc.connect(conn_str)
     
 cursor = conn.cursor()
 
